2d3a4a73c650186681fe2e550e6dd21cd6777415
Peter Amerkhanian <peteamer@gmail.com>
2019-03-08 11:46:31 -0500

    added route to regexs

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,9 +1,10 @@
 import re
 
-dateRegex = re.compile(r'\b\d{1,2}\sde\s[a-z]{5,11}\b(\sde\s20\d\d)?')
-timeRegex = re.compile(r'\d{1,2}:\d{1,2}', re.IGNORECASE)
-timeRegexDetailed = re.compile(r'\bmad(r)?ugada\b|\bmedianoche\b|\btarde\b|\bmediodía\b|\bmañana\b|\bnoche\b|\bprimeras horas de la mañana\b',
-                               re.IGNORECASE)
+date_regex = re.compile(r'\b\d{1,2}\sde\s[a-z]{5,11}\b(\sde\s20\d\d)?')
+time_regex = re.compile(r'\d{1,2}:\d{1,2}', re.IGNORECASE)
+time_regex_detailed = re.compile(r'\bmad(r)?ugada\b|\bmedianoche\b|\btarde\b|/'
+                                 r'\bmediodía\b|\bmañana\b|\bnoche\b|\bprimeras horas de la mañana\b',
+                                 re.IGNORECASE)
 
-
-
+route_regex = re.compile(r'(\s[A-Z][a-z]{0,10})?\s[A-Z]\w+\s?[-–][-]?\s?[A-Z]\w+(\s[A-Z][a-z]{0,10})?|/'
+                         r'(\s[A-Z][a-z]{0,10})?\s[A-Z]\w+\sa\s[A-Z]\w+(\s[A-Z][a-z]{0,10})?')
