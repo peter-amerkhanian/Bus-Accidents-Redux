1439214a37c0c5935fd79c90380fc832bf60b3de
Peter Amerkhanian <peteamer@gmail.com>
2019-03-11 15:33:39 -0500

    shifted more methods into Story class

--- filters/Story.py
+++ filters/Story.py
@@ -1,12 +1,27 @@
+from filters import get_date, get_time, get_deaths, get_route
+
+
 class Story:
     def __init__(self, url, date, title, summary, article, keywords):
         self.url = url
         self.date = date
-        # self.accident_date = accident_date
-        # self.accident_time = accident_time
-        # self.route = route
-        # self.deaths = deaths
         self.title = title
         self.summary = summary
         self.article = article
         self.keywords = keywords
+        self.accident_date = None
+        self.accident_time = None
+        self.route = None
+        self.deaths = None
+
+    def __repr__(self):
+        return f"Story title: {self.title}\nStory Summary: {self.summary}" \
+            f"\nStory url:{self.url}\nStory pub date: {self.date}" \
+            f"\nAccident date: {self.accident_date}\nAccident time: {self.accident_time}" \
+            f"\nAccident route: {self.route}\nDeaths: {self.deaths}\n"
+
+    def process(self):
+        self.accident_date = get_date(self)
+        self.accident_time = get_time(self)
+        self.route = get_route(self)
+        self.deaths = get_deaths(self)
--- run.py
+++ run.py
@@ -1,19 +1,9 @@
 import pickle
-from filters import get_time, get_date, get_route, get_deaths
 
 # TO DO:
 with open("filters/articles.pickle", "rb") as f:
     temp_data = pickle.load(f)
 
 for story in temp_data:
-    # story.accident_date = get_date(story)
-    # story.accident_time = get_time(story)
-    # story.route = get_route(story)
-    # story.deaths = get_deaths(story)
-    print(f"Story title: {story.title}\nStory Summary: {story.summary}\nStory url:{story.url}")
-    print(f"Story pub date: {story.date}")
-    print(f"Accident date: {get_date(story)}")
-    print(f"Accident time: {get_time(story)}")
-    print(f"Accident route: {get_route(story)}")
-    print(f"Deaths: {get_deaths(story)}")
-    print("\n")
+    story.process()
+    print(story)
