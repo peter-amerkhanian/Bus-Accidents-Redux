c22c46f20faa6064fd5e7b5a66d131eccfe04504
Peter Amerkhanian <peteamer@gmail.com>
2019-03-11 16:12:51 -0500

    export data to pandas df

--- filters/soup_retrieval.py
+++ filters/soup_retrieval.py
@@ -7,12 +7,13 @@ import pickle
 def get_soup(last_page):
     for x in range(1, last_page+1):
         page = requests.get(f"https://www.elcomercio.com/search/bus%20accidente%20ecuador/{x}")
-        soup = BeautifulSoup(page.content, "html.parser")
-        yield soup
+        page_soup = BeautifulSoup(page.content, "html.parser")
+        yield page_soup
 
 
-list_of_articles = []
-for soup in list(get_soup(10)):
-    list_of_articles.extend(list(get_stories(soup)))
+# list_of_articles = []
+# for soup in get_soup(10):
+#     list_of_articles.extend(get_stories(soup))
+list_of_articles = [get_stories(soup) for soup in get_soup(10)]
 with open('articles.pickle', 'wb') as f:
     pickle.dump(list_of_articles, f)
--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 import pickle
+import pandas as pd
 
 # TO DO:
 with open("filters/articles.pickle", "rb") as f:
     temp_data = pickle.load(f)
 
+data = []
 for story in temp_data:
     story.process()
-    print(story.to_dict())
+    print(story)
+    data.append(story.to_dict())
+
+df = pd.DataFrame(data)
+df.to_html('html/table.html')
+
+
