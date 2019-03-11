349f585555e76016ff6b4df1f44cd12a6b8975f7
Peter Amerkhanian <peteamer@gmail.com>
2019-03-11 14:30:10 -0500

    restructured

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,4 +1,4 @@
-from filters.regular_expressions import date_regex, time_regex, time_regex_detailed, route_regex, death_regex
-from filters.misc_helpers import get_first_match, spanish_to_english, fatal_keywords
+from filters.helpers import date_regex, time_regex, time_regex_detailed, route_regex, death_regex
+from filters.helpers import get_first_match, spanish_to_english, fatal_keywords
 from filters.text_filter_tools import get_time, get_date, get_route, get_deaths
-from filters.data_retrieval import get_stories
+from filters.text_retrieval import get_stories
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ with open("filters/articles.pickle", "rb") as f:
     temp_data = pickle.load(f)
 
 for story in temp_data:
-    print(f"Story title: {story.title}\nStory Summary: {story.summary}")
+    print(f"Story title: {story.title}\nStory Summary: {story.summary}\nStory url:{story.url}")
     print(f"Story pub date: {story.date}")
     print(f"Accident date: {get_date(story)}")
     print(f"Accident time: {get_time(story)}")
