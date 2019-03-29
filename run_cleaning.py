b749ae8718df1c7ba8cdbb206be8a9ce47c36c54
Peter Amerkhanian <peteamer@gmail.com>
2019-03-29 16:30:36 -0500

    flask app runs on processed data too

--- run_cleaning.py
+++ run_cleaning.py
@@ -1,6 +1,58 @@
-import pandas as pd
-from data_processing.data_cleaning import make_html_table
 import pickle
+from data_processing.data_cleaning import make_html_table
+import pandas as pd
+pd.options.display.float_format = '{:,.0f}'.format
+pd.set_option('display.max_colwidth', -1)
+
+data = pd.read_csv("data_processing/raw_data.csv")
 
 
-html_str = make_html_table(df, status="raw")
+def get_first_non_null(series):
+    series = series.tolist()
+    for x in series:
+        if x or type(x) != float:
+            return x
+
+
+def within_5(numbers_set):
+    numbers = list(numbers_set)
+    if len(numbers) == 1:
+        return True
+    baseline = numbers[0]
+    for number in numbers:
+        if abs(baseline - number) > 5:
+            return False
+    return True
+
+
+def max_two_routes(strings_set):
+    strings = list(strings_set)
+    if len(strings) <= 2:
+        return True
+
+
+for date in set(data.date):
+    _data = data.loc[data.date == date]
+    if len(_data) > 1:
+        death_set = set((_data['deaths'].tolist()))
+        print(death_set)
+        print(within_5(death_set))
+        route_set = set(([x.split('-')[-1].strip() if type(x) != float else x for x in _data['route']]))
+        print(route_set)
+        print(max_two_routes(route_set))
+        print('\n')
+        if max_two_routes(route_set) or within_5(death_set):
+            print('ya')
+            combination = _data.groupby('date', as_index=False).agg({'deaths': 'last',
+                                                                     'epi': 'first',
+                                                                     'route': get_first_non_null,
+                                                                     'time': get_first_non_null,
+                                                                     'url': get_first_non_null})
+            data = data[data.date != combination.date.tolist()[0]]
+            data = data.append(combination, sort=False)
+
+data = data.sort_values('date')
+data.to_csv("data_processing/processed_data.csv")
+html_str = make_html_table(data, status="processed")
+with open('data_processing/processed_data.pickle', 'wb') as f:
+    pickle.dump(html_str, f)
--- run_flask.py
+++ run_flask.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, Markup
 
 app = Flask(__name__)
 
-with open('data_processing/raw_data.pickle', 'rb') as f:
+with open('data_processing/processed_data.pickle', 'rb') as f:
     html_table = pickle.load(f)
 
 
