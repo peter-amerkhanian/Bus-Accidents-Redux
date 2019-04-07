e72014032d3d552c5c001160e585f330e6752fde
Peter Amerkhanian <peteamer@gmail.com>
2019-04-07 15:59:27 -0500

    altered table view in html

--- run_flask.py
+++ run_flask.py
@@ -5,10 +5,16 @@ from flask import Flask, render_template, Markup
 app = Flask(__name__)
 
 with open('data_processing/processed_data.pickle', 'rb') as f:
-    html_table = pickle.load(f)
+    html_table_processed = pickle.load(f)
+
+with open('data_processing/raw_data.pickle', 'rb') as f:
+    html_table_raw = pickle.load(f)
 
 
 @app.route('/')
-def hello_world(table=Markup(html_table)):
-    return render_template('table.html', table=table)
+def hello_world(table_processed=Markup(html_table_processed),
+                table_raw=Markup(html_table_raw)):
+    return render_template('table.html',
+                           table_processed=table_processed,
+                           table_raw=table_raw)
 
